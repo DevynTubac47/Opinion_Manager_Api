440c6ea7367c0ed4893d3c7c27db225bbbcde0f0
Informatica <informatica@alumno.local>
2025-02-17 11:51:17 -0600

    feat: Crear cuenta y login

--- configs/server.js
+++ configs/server.js
@@ -6,6 +6,7 @@ import helmet from "helmet"
 import morgan from "morgan"
 import { dbConnection } from "./mongo.js"
 import apiLimiter from "../src/middlewares/rate-limit-validator.js"
+import authRoutes from "../src/auth/auth.router.js"
 
 const middlewares = (app) => {
     app.use(express.urlencoded({extended: false}))
@@ -17,7 +18,7 @@ const middlewares = (app) => {
 }
 
 const routes = (app) =>{
-
+    app.use("/opinionManager/v1/auth",authRoutes)
 }
 
 const conectarDB = async () =>{
